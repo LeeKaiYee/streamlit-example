170a9eda4f7459466b87361483764b46ff8ef1e1
LeeKaiYee <111936652+LeeKaiYee@users.noreply.github.com>
2022-08-24 13:06:06 +0800

    Update Iris_table.py

--- Iris_table.py
+++ Iris_table.py
@@ -6,3 +6,8 @@ st.header("Iris")
 st.write(pd.DataFrame({
     'Iris': ['Iris-setosa', 'Iris-versicolor', 'Iris-virginaca']
 }))
+
+from PIL import Image
+image = Image.open('sunrise.jpg')
+
+st.image(image, caption='Sunrise by the mountains')
